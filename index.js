6f556f6e727f91369dc2e0b554a607c823ade7bd
Julian Lobon <julitolos6@gmail.com>
2024-08-05 15:55:31 -0500

    obteniendo notas por id

--- index.js
+++ index.js
@@ -26,10 +26,18 @@ app.get('/', (require, res) => {
   res.send("<h1>Hola mundo</h1>")
 })
 
-app.get('/api/notas', (require, res) => {
-  res.json(notas)
-})
 
+app.get('/api/notas/:id', (request, res) => { 
+  const id =Number(request.params.id)
+  console.log(id)
+  const nota = notas.find(nota => {
+    console.log(nota.id, typeof nota.id , id, typeof id, nota.id === id)
+    return nota.id === id
+  } )
+  console.log(nota)
+  res.json(nota)
+})
+ 
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`corriendo el puerto ${PORT}`)
