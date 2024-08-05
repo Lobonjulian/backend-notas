b1bf66e6b209911a632df479b1817e281ccf187d
Julian Lobon <julitolos6@gmail.com>
2024-08-05 14:52:55 -0500

    instalando express

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const http = require('http')
 
+let notas = [
+  {
+    id: 1,
+    contenido: "HTML es facil",
+    import: true
+  },
+  {
+    id: 2,
+    contenido: "los Navegadores solo ejecutan JavaScript", 
+    import: false
+  },
+  {
+    id: 3,
+    contenido: "GET y POST son los mejores peticiones HTTP",
+    import: true
+  }
+
+]
+
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-type': 'text/plain' })
-  response.end('Hola mundo')
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(notas))
 })
 
 const PORT = 3000
