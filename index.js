492ef660b4a792b5d4d7e299e8e8aeedcb93d009
Julian Lobon <julitolos6@gmail.com>
2024-08-06 10:26:18 -0500

    creando la lógica de el método Post

--- index.js
+++ index.js
@@ -1,73 +1,80 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 let notas = [
   {
     id: 1,
-    contenido: "HTML es fácil",
-    import: true
+    contenido: 'HTML es fácil',
+    important: true,
   },
   {
     id: 2,
-    contenido: "los Navegadores solo ejecutan JavaScript", 
-    import: false
+    contenido: 'los Navegadores solo ejecutan JavaScript',
+    important: false,
   },
   {
     id: 3,
-    contenido: "GET y POST son los mejores peticiones HTTP",
-    import: true
-  }
-
-]
+    contenido: 'GET y POST son los mejores peticiones HTTP',
+    important: true,
+  },
+];
 
 //raiz de la app
 app.get('/', (require, res) => {
-  res.send("<h1>Hola mundo</h1>")
-})
- 
+  res.send('<h1>Hola mundo</h1>');
+});
+
 //obtencion de notas
 app.get('/api/notas', (request, res) => {
-  res.json(notas)
-})
+  res.json(notas);
+});
 
-
-app.get('/api/notas/:id', (request, res) => { 
-  const id =Number(request.params.id)
-  const nota = notas.find(nota =>  nota.id === id)
+app.get('/api/notas/:id', (request, res) => {
+  const id = Number(request.params.id);
+  const nota = notas.find((nota) => nota.id === id);
 
   if (nota) {
-    res.json(nota)
+    res.json(nota);
   } else {
-    res.status(404).end()
+    res.status(404).end();
   }
-})
+});
 
 // eliminar Nota
-app.delete('/api/notas/:id', (request, res) => { 
-  const id = Number(request.params.id)
-  notas = notas.filter(nota => nota.id !== id)
+app.delete('/api/notas/:id', (request, res) => {
+  const id = Number(request.params.id);
+  notas = notas.filter((nota) => nota.id !== id);
 
-  res.status(204).end()
-})
+  res.status(204).end();
+});
 
 //recibir información
+const generarId = () => {
+  const maxId = notas.length > 0 ? Math.max(...notas.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
 app.post('/api/notas', (request, res) => {
-  const maxId = notas.length > 0
-    ? Math.max(...notas.map(n => n.id))
-    : 0
+  const body = request.body;
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'El contenido es obligatorio',
+    });
+  }
 
-  const nota = request.body
-  nota.id = maxId +1
+  const nota = {
+    contenido: body.content,
+    important: Boolean(body.important) || false,
+    id: generarId(),
+  };
+  notas = notas.concat(nota);
 
-  notas = notas.concat(nota)
-  
-  res.json(nota)
-})
+  res.json(nota);
+});
 
- 
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`corriendo el puerto ${PORT}`)
-})
+  console.log(`corriendo el puerto ${PORT}`);
+});
